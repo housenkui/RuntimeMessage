6ef2c1617843f900658ba617550aff32b9602c4f
Daves_Code_Hou <840737320@qq.com>
2017-03-25 11:33:59 +0800

    Initial Commit

---
+++ RuntimeMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeMessage
+//
+//  Created by Code_Hou on 2017/3/25.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeMessage
+//
+//  Created by Code_Hou on 2017/3/25.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
